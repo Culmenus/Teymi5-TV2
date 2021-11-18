a6fc749e9ed485279d56500432bdc63aa9e1cf99
Culmenus <odduraskell@gmail.com>
2021-11-18 00:01:38 +0000

    eitthvað að gerast aðeins of soðinn

--- verk3/SequenceNeural.py
+++ verk3/SequenceNeural.py
@@ -1,112 +1,141 @@
+import os
 import torch as T
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.distributions.normal import Normal
 import numpy as np
 
-
-# er að fylja þessu tutoriali https://youtu.be/wc-FxNENg9U
-class DeepQNetwork(nn.module):
-    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions):
-        super(DeepQNetwork, self).__init__()
+class CriticNetwork(nn.Module):
+    def __init__(self, beta, input_dims, fc1_dims, fc2_dims, n_actions,
+            name, chkpt_dir='tmp/sac'):
+        super(CriticNetwork, self).__init__()
         self.input_dims = input_dims
         self.fc1_dims = fc1_dims
         self.fc2_dims = fc2_dims
         self.n_actions = n_actions
-        #extend for 2d matrix https://www.youtube.com/watch?v=wc-FxNENg9U&t=267s
-        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims) 
+        self.name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac')
+
+        # I think this breaks if the env has a 2D state representation
+        self.fc1 = nn.Linear(self.input_dims[0] + n_actions, self.fc1_dims)
         self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
-        self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
-        self.optimizer = optim.Adam(self.parameters(), lr=lr)
-        self.loss = nn.MSELoss()
-        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.q1 = nn.Linear(self.fc2_dims, 1)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=beta)
+        self.device = T.device('cpu')
+
         self.to(self.device)
-    
-    def forward(self, state):
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        actions = self.fc3(x)
-        
-        return actions
-    
-class Agent():
-    def __init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions,
-            max_mem_size=100000, eps_end=0.01, eps_dec=5e-4):
-        self.gamma = gamma
-        self.epsilon = epsilon
-        self.lr = lr
+
+    def forward(self, state, action):
+        q1_action_value = self.fc1(T.cat([state, action], dim=1))
+        q1_action_value = F.relu(q1_action_value)
+        q1_action_value = self.fc2(q1_action_value)
+        q1_action_value = F.relu(q1_action_value)
+
+        q1 = self.q1(q1_action_value)
+
+        return q1
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+class ActorNetwork(nn.Module):
+    def __init__(self, alpha, input_dims, fc1_dims, fc2_dims,
+            n_actions, name, chkpt_dir='./'):
+        super(ActorNetwork, self).__init__()
         self.input_dims = input_dims
-        self.batch_size = batch_size
-        #makes it easy to select action at random
-        self.action_space = [i for i in range(n_actions)] 
-        self.mem_size = max_mem_size 
-        self.mem_cntr = 0
-        self.eps_end = eps_end
-        self.eps_dec = eps_dec
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac')
+        self.reparam_noise = 1e-6
+
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.mu = nn.Linear(self.fc2_dims, self.n_actions)
+        self.sigma = nn.Linear(self.fc2_dims, self.n_actions)
+
+        self.device = T.device( 'cpu')
+        self.to(self.device)
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+
         
-        self.Q_eval = DeepQNetwork(self.lr, n_actions=n_actions, input_dims=input_dims,
-                                   fc1_dims=256, fc2_dims=256)
-        
-        self.state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
-        self.new_state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
-        
-        self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
-        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
-        self.terminal_memory = np.zeros(self.mem_size, dype=np.bool)
-        
-    def store_transition(self, state, action, reward, state_, isTerminal):
-        index = self.mem_cntr % self.mem_size
-        self.state_memory[index] = state
-        self.new_state_memory[index] = state_
-        self.reward_memory[index] = reward
-        self.action_memory[index] = action
-        self.terminal_memory[index] = isTerminal
-        
-        self.mem_cntr += 1
-        
-        
-    #epsilon greedy
-    def choose_action(self, observation):
-        if np.random.random() > self.epsilon:
-            state = T.tensor([observation]).to(self.Q_eval.device)
-            actions = self.Q_eval.forward(state)
-            action = T.argmax(actions).item()
+
+    def forward(self, state):
+        prob = self.fc1(state.float())
+        prob = F.relu(prob)
+        prob = self.fc2(prob)
+        prob = F.relu(prob)
+
+        mu = self.mu(prob)
+        #sigma = T.sigmoid(self.sigma(prob))
+        sigma = self.sigma(prob)
+        sigma = T.clamp(sigma, min=self.reparam_noise, max=1) 
+        # authors use -20, 2 -> doesn't seem to work for my implementation
+
+        return mu, sigma
+
+    def sample_normal(self, state, reparameterize=True):
+        mu, sigma = self.forward(state)
+        probabilities = T.distributions.Normal(mu, sigma)
+
+        if reparameterize:
+            actions = probabilities.rsample() # reparameterizes the policy
         else:
-            action = np.random.choice(self.action_space)
-        
-        return action
-    
-    def learn(self):
-        if self.mem_cntr < self.batch_size:
-            return
-        
-        self.Q_eval.eptimizer.zero_grad()
-        
-        max_mem = min(self.mem_cntr, self.meme_size)
-        batch = np.random.choice(max_mem, self.batch_size, replace=False)
-        
-        batch_index = np.arange(self.batch_size, dtype=np.int32)
-        
-        state_batch = T.tensor(self.state_memory[batch]).to(self.Q_eval.device)
-        new_state_batch = T.tensor(self.new_state_memory[batch]).to(self.Q_eval.device)
-        reward_batch = T.tensor(self.reward_memory[batch]).to(self.Q_eval.device)
-        terminal_batch = T.tensor(self.terminal_memory[batch]).to(self.Q_eval.device)
-        
-        action_batch = self.action_memory[batch]
-        
-        q_eval = self.Q_eval.forward(state_batch)[batch_index, action_batch]
-        q_next = self.Q_eval.forward(new_state_batch)
-        q_next[terminal_batch] = 0.0
-        
-        q_target = reward_batch + self.gamma * T.max(q_next, dim=1)[0]
-        
-        loss = self.Q_eval.loss(q_target, q_eval).to(self.Q_eval.device)
-        self.Q_eval.optimizer.step()
-        
-        self.epsilon = self.epsilon - self.eps_dec if self.epsilon > self.eps_min \
-                        else self.eps_min
-        
-        
-        
-        
-        
+            actions = probabilities.sample()
+
+        action = T.tanh(actions)*T.tensor(self.max_action).to(self.device) 
+        log_probs = probabilities.log_prob(actions)
+        log_probs -= T.log(1-action.pow(2) + self.reparam_noise)
+        log_probs = log_probs.sum(1, keepdim=True)
+
+        return action, log_probs
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+class ValueNetwork(nn.Module):
+    def __init__(self, beta, input_dims, fc1_dims, fc2_dims,
+            name, chkpt_dir='tmp/sac'):
+        super(ValueNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac')
+
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.v = nn.Linear(self.fc2_dims, 1)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=beta)
+        self.device = T.device('cpu')
+
+        self.to(self.device)
+
+    def forward(self, state):
+        state_value = self.fc1(state.float())
+        state_value = F.relu(state_value)
+        state_value = self.fc2(state_value)
+        state_value = F.relu(state_value)
+
+        v = self.v(state_value)
+
+        return v
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
